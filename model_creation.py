0f87b7573091baf1cb375e77d0d70417b6849f8d
grahamstream <grahamstream@gmail.com>
2025-02-16 16:29:18 -0800

    Refactor logistic regression grid and add evaluation method
    Revised logistic regression parameter grid for enhanced flexibility with different solvers. Added a new `log_reg` function to train, validate, and evaluate the model using f1, recall, and precision metrics. The function also appends predictions and accuracy to the dataset.

--- model_creation.py
+++ model_creation.py
@@ -4,14 +4,29 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import GridSearchCV, train_test_split
+from sklearn.metrics import f1_score, recall_score, precision_score
 
 
-logreg_param_grid = {
-    'penalty': ['l1', 'l2', 'elasticnet', 'none'],
-    'C': [0.01, 0.1, 1.0, 10.0, 100.0],
-    'solver': ['lbfgs', 'liblinear', 'saga'],
-    'max_iter': [100, 200, 500]
-}
+logreg_param_grid = [
+    {
+        'penalty': ['l1', 'l2'],
+        'C': [0.01, 0.1, 1.0, 10.0, 100.0],
+        'solver': ['liblinear'],
+        'max_iter': [100, 200, 500]
+    },
+    {
+        'penalty': ['l2', 'none'],
+        'C': [0.01, 0.1, 1.0, 10.0, 100.0],
+        'solver': ['lbfgs'],
+        'max_iter': [100, 200, 500]
+    },
+    {
+        'penalty': ['l1', 'l2', 'elasticnet', 'none'],
+        'C': [0.01, 0.1, 1.0, 10.0, 100.0],
+        'solver': ['saga'],
+        'max_iter': [100, 200, 500]
+    }
+]
 
 svc_param_grid = {
     'C': [0.01, 0.1, 1.0, 10, 100],
@@ -30,3 +45,32 @@ clf_param_grid = {
     'criterion': ['gini', 'entropy']
 
 }
+
+def log_reg(data, target_col):
+
+    df = pd.DataFrame(data)
+    x= df.drop(columns=target_col)
+    y = df[target_col]
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.25, random_state=32)
+
+    grid_search_logreg = GridSearchCV(
+        estimator=LogisticRegression(),
+        param_grid=logreg_param_grid,
+        scoring='f1',
+        cv=5
+    )
+
+    grid_search_logreg.fit(x_train, y_train)
+
+    y_pred = grid_search_logreg.predict(x_test)
+
+    f1 = f1_score(y_test, y_pred)
+    recall = recall_score(y_test, y_pred)
+    precision = precision_score(y_test, y_pred)
+
+    df_final = df.copy()
+    df_final['prediction'] = grid_search_logreg.predict(df.drop(columns=target_col))
+    df_final['accuracy'] = (df_final[target_col] == df_final['prediction']).astype(int)
+
+    return df_final.to_dict('records'), f1, recall, precision
